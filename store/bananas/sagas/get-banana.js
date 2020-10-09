669b8569b25bc09dfda0d6270fb3c2c420daa9c0
Diego Andai <diego.andai@platan.us>
2020-10-09 04:15:34 -0300

    saga api call

--- store/bananas/sagas/get-banana.js
+++ store/bananas/sagas/get-banana.js
@@ -1,12 +1,18 @@
-import { put, takeLatest } from 'redux-saga/effects';
+import { call, put, takeLatest } from 'redux-saga/effects';
+import api from '../../../api';
 
 import { actions as bananaActions } from '../slice';
 
 function *harvestBanana({ payload }) {
   const { harvester } = payload;
   yield put(bananaActions.harvestStart());
-  // api call
-  yield put(bananaActions.harvestFailure());
+  const { data: { known } } = yield call(api.checkKnown, harvester);
+  if (known) {
+    yield put(bananaActions.harvestSuccess());
+    yield put(bananaActions.addBanana());
+  } else {
+    yield put(bananaActions.harvestFailure());
+  }
   yield put(bananaActions.harvestEnded());
 }
 
